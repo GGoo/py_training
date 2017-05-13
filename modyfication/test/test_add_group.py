577a82030dd9ebeae144a82039d70d159431303f
GGoo <piekarska@live.com>
2017-05-13 08:13:09 +0200

    ex 8

--- modyfication/test/test_add_group.py
+++ modyfication/test/test_add_group.py
@@ -3,7 +3,7 @@ from modyfication.model.group import Group
 
 
 def test_add_group(app):
-    app.group.create(Group(name="group1", header="group1", footer="group1"))
+    app.group.create(Group(name="ex8", header="ex8 header", footer="ex8 footer"))
 
 def test_add_empy_group(app):
     app.group.create(Group(name="", header="", footer=""))
